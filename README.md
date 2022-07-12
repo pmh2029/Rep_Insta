177dff8c917869ba55aa3ffd82999d168ef71ee3
pmh <hieu.pm2029@gmail.com>
2022-07-12 21:43:03 +0700

    switch to gr account

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Rep_Insta
+
+1. yarn / npm i
+
+2. yarn add moment / npm i moment
+
+3. yarn start / npm start
--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,15 +1,6 @@
-// export const apiKey = 'AIzaSyB2PEn_2YIGYUSP99Ru7iclENiE4FLuo8A';
-// export const authDomain = 'instagram-53a21.firebaseapp.com';
-// export const projectId = 'instagram-53a21';
-// export const storageBucket = 'instagram-53a21.appspot.com';
-// export const messagingSenderId = '987962045619';
-// export const appId = '1:987962045619:web:b99d490ef17e9b26846cc6';
-
-export const apiKey = 'AIzaSyB5Wa_mF5Rm5VbBSaTcSGMP8_t-pE5YDXw';
-export const authDomain = 'baboya-2c8fd.firebaseapp.com';
-export const databaseURL = 'https://baboya-2c8fd-default-rtdb.firebaseio.com';
-export const projectId = 'baboya-2c8fd';
-export const storageBucket = 'baboya-2c8fd.appspot.com';
-export const messagingSenderId = '711231584519';
-export const appId = '1:711231584519:web:e44158d5a4b66e6d6a6da2';
-export const measurementId = 'G-1FD4FY12QX';
+export const apiKey = 'AIzaSyB5Wa_mF5Rm5VbBSaAIzaSyARKOAQf42BF7DpWvKPfqLI34O7VV0_TqQ';
+export const authDomain = 'jp-itss.firebaseapp.com';
+export const projectId = 'jp-itss';
+export const storageBucket = 'jp-itss.appspot.com';
+export const messagingSenderId = '723463735444';
+export const appId = '1:723463735444:web:5ea5d3ed337066f9fe4c81';
